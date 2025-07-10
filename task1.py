b2885ccec16dc5b2b5efe6e7f5d80261d02ffbe4
jitendra-2608 <jitendrashriwas45@gmail.com>
2025-07-10 20:25:25 +0530

    Add files via upload
    Signed-off-by: jitendra-2608 <jitendrashriwas45@gmail.com>

---
+++ task1.py
@@ -0,0 +1,10 @@
+number1 = eval(input("enter the num1 "))
+number2 =eval(input("eneter the num2 "))
+sum=number1 +number2
+print(" the sum of number ",sum)
+division=number1/number2 
+print(" the division of number ",division)
+multiply= number1*number2
+print( "the multiplication of number",multiply)
+substract=number1 -number2
+print("the substraction of number ", substract) 
