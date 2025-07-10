6205c31ab9b7f28a01dbd101465bfa3cfb29d8a2
jitendra-2608 <jitendrashriwas45@gmail.com>
2025-07-10 20:20:04 +0530

    Delete task2.py
    Signed-off-by: jitendra-2608 <jitendrashriwas45@gmail.com>

--- task2.py
+++
@@ -1,4 +0,0 @@
-# theprogram of greeting the 
-first_name= input(" the first name ")
-last_name = input("the last name ")
-print ("hii ",first_name + last_name,"we  are welcome  you in python coder club")
